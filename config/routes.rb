d88c03335c431e1cca1266fe34172838317e86cc
Jacqui Nelson <jnelson@cotingasoft.com>
2017-06-12 23:25:38 -0500

    Add login

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
 Rails.application.routes.draw do
 
-  resources :users
+  get 'sessions/new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'static_pages#home'
 
-  get  '/about',   to: 'static_pages#about'
+  get '/about',   to: 'static_pages#about'
+  get '/signup',  to: 'users#new'
+  get '/login',   to: 'sessions#new'
+  post '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
+  resources :users
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
