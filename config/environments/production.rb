0a4880069bb686ef200c9653e0ad51b411d076cf
Jacqui Nelson <jnelson@cotingasoft.com>
2017-07-02 13:29:04 -0500

    Fix url in password reset email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,8 +70,11 @@ Rails.application.configure do
     :address => ENV['MAILGUN_SMTP_SERVER'],
     :domain => ENV['MAILGUN_DOMAIN'],
     :user_name => ENV['MAILGUN_SMTP_LOGIN'],
-    :password => ENV['MAILGUN_SMTP_PASSWORD']
+    :password => ENV['MAILGUN_SMTP_PASSWORD'],
   }
+  host = 'nolaorgangrinders.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  config.action_controller.default_url_options = { host:  host}
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
