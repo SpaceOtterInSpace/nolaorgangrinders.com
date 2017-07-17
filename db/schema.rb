d334f9ac2b5af3dbbfbb2a4c553d54ef3848d661
Jacqui Nelson <jnelson@cotingasoft.com>
2017-07-16 20:31:25 -0500

    Track user's addresses

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170702030206) do
+ActiveRecord::Schema.define(version: 20170717012128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20170702030206) do
     t.datetime "activated_at"
     t.string   "reset_digest"
     t.datetime "reset_sent_at"
+    t.string   "address"
   end
 
 end
