f4d648bab0715383fcdef1643b4f6ea34e2c966f
Jacqui Nelson <jnelson@cotingasoft.com>
2017-07-23 17:45:31 -0500

    Show how much of dues have been paid

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,26 @@ class Order < ActiveRecord::Base
   belongs_to :payment
 
   def paid?
-    !payment.nil?
+    amount_paid == amount
   end
+
+  def amount_paid
+    paid_for_this_order = user.paid - previous_order_cost
+
+    if paid_for_this_order > self.amount
+      return self.amount
+    else
+      return [0, paid_for_this_order].max
+    end
+  end
+
+  def previous_orders
+    orders = user.orders
+    previous_orders = orders.select { |order| order.due_date < self.due_date }
+  end
+
+  def previous_order_cost
+    previous_orders.map(&:amount).sum
+  end
+
 end
