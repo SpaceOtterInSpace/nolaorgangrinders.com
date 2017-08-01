fe4fd5b007a5a335129044e4ce9c86674feace8a
Jacqui Nelson <jnelson@cotingasoft.com>
2017-07-31 19:54:54 -0500

    Add phone numbers and edit phone and address for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170727005739) do
+ActiveRecord::Schema.define(version: 20170801004706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20170727005739) do
     t.datetime "reset_sent_at"
     t.string   "address"
     t.text     "note"
+    t.string   "phone"
   end
 
 end
