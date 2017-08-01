33f0c7269f777c09a1dd0db2604c52f0de757829
Jacqui Nelson <jnelson@cotingasoft.com>
2017-07-31 19:08:07 -0500

    Fix error on password reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -53,7 +53,7 @@ class PasswordResetsController < ApplicationController
 
   # Checks expiration of reset token.
   def check_expiration
-    if @user && @user.password_reset_expired?
+    if @user.nil? || @user.password_reset_expired?
       flash[:danger] = "Password reset has expired."
       redirect_to new_password_reset_url
     end
