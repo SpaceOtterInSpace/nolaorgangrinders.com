4cefc7c19d7ede1f4938d869f5883e1e92e5d833
Jacqui Nelson <jnelson@cotingasoft.com>
2017-08-05 17:53:09 -0500

    Add active and nicknames to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170801004706) do
+ActiveRecord::Schema.define(version: 20170805225130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,8 @@ ActiveRecord::Schema.define(version: 20170801004706) do
     t.string   "address"
     t.text     "note"
     t.string   "phone"
+    t.boolean  "active"
+    t.string   "nickname"
   end
 
 end
